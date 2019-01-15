f90450c944e51dbf1362a51c7fa07281da46e3d0
Dustin Cummings <dustincummings15@gmail.com>
2019-01-15 15:29:51 -0500

    added dropdown list to event edit

--- src/app/components/event/event-edit/event-edit.component.ts
+++ src/app/components/event/event-edit/event-edit.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { Event } from 'src/app/models/Event';
 import { EventService } from 'src/app/services/event.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FoodsService } from 'src/app/services/foods.service';
+import { CustomerService } from 'src/app/services/customer.service';
 @Component({
   selector: 'app-event-edit',
   templateUrl: './event-edit.component.html',
@@ -10,10 +12,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class EventEditComponent implements OnInit {
   event: Event;
+  foodData: food[] = [];
+  customerData: customer[] = [];
 
   editEventForm: FormGroup;
   
-  constructor(private _form:FormBuilder,private _eventService: EventService, private _ar:ActivatedRoute,private _router: Router) {
+  constructor(private _form:FormBuilder,private _eventService: EventService, private _ar:ActivatedRoute,private _router: Router, private _foodService: FoodsService, private _customerService: CustomerService) {
     this._ar.paramMap.subscribe(p =>{
       this._eventService.getEvent(p.get('id')).subscribe((singleEvent:Event) =>{
         this.event = singleEvent;
@@ -23,6 +27,25 @@ export class EventEditComponent implements OnInit {
    }
 
   ngOnInit() {
+    this._foodService.getFoods().subscribe((foods: any[]) => {
+      foods.forEach(foodSingle => {
+        var foodFormatted: food = {
+          name: foodSingle.name,
+          id: foodSingle.foodID
+        };
+        this.foodData.push(foodFormatted)
+      });
+      this._customerService.getCustomers().subscribe((customers:any[])=>{
+        customers.forEach(customerSingle =>{
+          var customerFormatted: customer = {
+            custFirstName: customerSingle.custFirstName,
+            custLastName: customerSingle.custLastName,
+            custID: customerSingle.custID
+          };
+          this.customerData.push(customerFormatted)
+        })
+      })
+    })
   }
   createForm() {
     this.editEventForm = this._form.group({
@@ -30,7 +53,7 @@ export class EventEditComponent implements OnInit {
       OwnerID: new FormControl(this.event.ownerID),
       Location: new FormControl(this.event.location),
       NumberOfPeople: new FormControl(this.event.numberOfPeople),
-      FoodId: new FormControl(this.event.foodID),
+      FoodID: new FormControl(this.event.foodID),
       AdditionalNotes: new FormControl(this.event.additionalNotes),
       DateOfEvent: new FormControl(this.event.dateOfEvent),   
       CustID: new FormControl(this.event.custID), 
@@ -42,7 +65,7 @@ export class EventEditComponent implements OnInit {
       ownerID: form.value.OwnerID,
       location: form.value.Location,
       numberOfPeople: form.value.NumberOfPeople,
-      foodID: form.value.FoodId,
+      foodID: form.value.FoodID,
       additionalNotes: form.value.AdditionalNotes,
       dateOfEvent: form.value.DateOfEvent,
       custID: form.value.CustID,
@@ -51,6 +74,13 @@ export class EventEditComponent implements OnInit {
       this._router.navigate(['/events']);
     });
   }
-
-
+}
+export interface food {
+  name: string
+  id: number
+}
+export interface customer{
+  custFirstName: string
+  custLastName: string
+  custID: number
 }
