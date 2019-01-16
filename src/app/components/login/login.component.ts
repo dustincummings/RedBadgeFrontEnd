4e04edab49303a78a6fc8c9437d170b1ec20c48d
Marquise Grayson <gmarquise47@gmail.com>
2019-01-16 16:59:39 -0500

    clean up

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{FormBuilder, FormGroup, FormControl,} from'@angular/forms';
-import { AuthService } from '../../auth.service'; 
+import { AuthService } from '../../services/auth.service'; 
 
 @Component({
   selector: 'app-login',
