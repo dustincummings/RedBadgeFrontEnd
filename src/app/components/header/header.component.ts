c3cc3cb98bee9267953fb166e3ed3bb51d4fab43
Dustin Cummings <dustincummings15@gmail.com>
2019-01-21 09:02:30 -0500

    hide buttons when logged in

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,24 +10,20 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 })
 export class HeaderComponent implements OnInit {
   username: string;
-  isLoggedIn: any;
+  isLoggedIn: boolean;
 
-  constructor(private _authService: AuthService, private _router: Router, private _jwtHelper: JwtHelperService) {
-    if (localStorage.getItem('id_token') !== null){
-      this.isLoggedIn = true;
-    }
-    else{
-      this.isLoggedIn =false;
-
-    }
-   }
+  constructor(private _authService: AuthService, private _router: Router, ) {}
+  
 
   ngOnInit() {
-   this.username = this._jwtHelper.decodeToken(localStorage.getItem('id_token')).unique_name
+    this._authService.isLoggedIn.subscribe((loggedInUserStatus: boolean) =>{
+    this.isLoggedIn = loggedInUserStatus;
+    });
 }
 
   onLogout() {
     this._authService.logout();
+    this.isLoggedIn =false;
     this._router.navigate(['/login']);
   }
 
