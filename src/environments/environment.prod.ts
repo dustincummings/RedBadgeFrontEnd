eef8be411c0d4837c9745182013caba5f8d70a66
Stephanie Mizelle <samizelle@gmail.com>
2019-01-22 10:50:23 -0500

    add api to url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,11 @@ switch (window.location.hostname) {
   // this is the deployed angular application
   case 'dc-cateringcoordinator.herokuapp.com':
     // this is the full url of your deployed API
-    APIURL = 'https://cateringcoordinator.azurewebsites.net/'
+    APIURL = 'https://cateringcoordinator.azurewebsites.net/api'
     break;
   default:
     // this is the local host name of your API
-    APIURL = 'https://cateringcoordinator.azurewebsites.net/';
+    APIURL = 'https://cateringcoordinator.azurewebsites.net/api';
 }
 
 export const environment = {
